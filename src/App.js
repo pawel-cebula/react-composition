915f6549724facc12c8d11976166af0ad84e6665
Pawel Cebula <hi@pawelcebula.com>
2021-10-03 17:06:50 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Card = ({ title, subTitle, children, actions = ['like', 'comment'] }) => (
+  <div class="card">
+    <div class="card-heading">
+      <h2>{title}</h2>
+      <p>{subTitle}</p>
+    </div>
+    <div class="card-main">{children}</div>
+    <div class="card-actions">
+      {actions.map((action) => (
+        <button>{action}</button>
+      ))}
+    </div>
+  </div>
+);
+
+const PhotoCard = ({ src, alt, children, ...rest }) => (
+  <Card {...rest}>
+    <img src={src} alt={alt} />
+    {children}
+  </Card>
+);
+
+const TextCard = ({ paragraphs, ...rest }) => (
+  <Card {...rest}>
+    {paragraphs.map((paragraph) => (
+      <p>{paragraph}</p>
+    ))}
+  </Card>
+);
+
+const App = () => {
+  const ipaInfo = [
+    'India pale ale (IPA) is a hoppy beer style within the broader category of pale ale.',
+    'The style of pale ale which became known as India pale ale was widespread in England by 1815, and would grow in popularity, notably as an export beer shipped to India and elsewhere.',
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>React composition</h1>
+      <div class="cards">
+        <Card title="Craft beer" subTitle="Most popular styles">
+          <ul>
+            <li>IPA</li>
+            <li>Sour</li>
+            <li>Stout</li>
+          </ul>
+        </Card>
+        <PhotoCard
+          title="NEIPA"
+          subTitle="New England India Pale Ale"
+          src="https://www.beercartel.com.au/product_images/uploaded_images/neipa2017-cp.jpg"
+          alt="NEIPA photo"
         >
-          Learn React
-        </a>
-      </header>
+          <p>New England IPAs are beers that are purposely hazy or cloudy.</p>
+        </PhotoCard>
+        <TextCard
+          title="IPA"
+          subTitle="India Pale Ale"
+          paragraphs={ipaInfo}
+          actions={['save']}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
